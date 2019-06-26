a69d6e0564909e9647d72e7b60c018bd96efdda3
Francisco J. Seva <franciscojseva@gmail.com>
2019-06-26 14:29:33 +0200

    Updates

---
+++ precache-manifest.8a7d9775fa3627b4ca740f5eb2ca68b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e35de5df9032ee52cc6cb69c58b4f37",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "d6bb69a1aaffbffb72fa",
+    "url": "/react-shop/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d26b8fc1d32e798de91d",
+    "url": "/react-shop/static/js/2.fe27daa4.chunk.js"
+  },
+  {
+    "revision": "d6bb69a1aaffbffb72fa",
+    "url": "/react-shop/static/js/main.61bb9084.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  }
+]);
